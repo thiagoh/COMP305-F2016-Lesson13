1817d6ff2fd3d82fd5d4e35570ac93709d2a3d4b
Thiago Andrade <thiagoh@gmail.com>
2016-12-10 20:13:57 -0500

    first commit

---
+++ Assets/_Scripts/CoinController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour {
+    
+  public AudioSource coinSound;
+  private WaitForSeconds waitTime = new WaitForSeconds(0.15f);
+  // Use this for initialization
+  void Start() {
+		
+  }
+	
+  // Update is called once per frame
+  void Update() {
+	    
+    transform.Rotate(Vector3.forward * Time.deltaTime * 360);
+  }
+
+  void OnCollisionEnter(Collision other) {
+
+    if (other.gameObject.CompareTag("Player")) {
+      StartCoroutine(playCoinSound());
+    }
+  }
+
+  // Coroutine
+  IEnumerator playCoinSound() {
+    coinSound.Play();
+    yield return waitTime;
+    gameObject.SetActive(false);
+  }
+
+}
---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+  public Transform spawnPoint;
+  public GameObject player;
+  public GameObject coin;
+
+  private static int MAX_COINS = 20;
+
+  private List<GameObject> coinPool;
+
+  // Use this for initialization
+  void Start() {
+    Instantiate(player);
+    player.transform.position = spawnPoint.position;
+    coinPool = new List<GameObject>();
+    buildCoinPool();
+  }
+	
+  // Update is called once per frame
+  void Update() {
+    placeCoins();
+  }
+
+  void buildCoinPool() {
+
+    for (int i = 0; i < MAX_COINS; i++) {
+    
+      GameObject _coin = (GameObject)Instantiate(coin);
+      _coin.SetActive(false);
+      coinPool.Add(_coin);
+    }
+  }
+
+  private void placeCoins() {
+
+    foreach (GameObject _coin in coinPool) {
+      if (!_coin.activeInHierarchy) {
+        _coin.SetActive(true);
+        _coin.transform.position = new Vector3(Random.Range(-20f, 20f), 20f, Random.Range(-20f, 20f));
+      }
+    }
+  }
+}
